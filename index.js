c80f38c22dfc3dedeba15f8b40b0dbe0f8a070a0
rois90 <roissm90@gmail.com>
2024-01-15 12:11:34 +0100

    fix route usersFF

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const MongoStore = require('connect-mongo');
 require('./authentication/passport');
 const cors = require('cors');
 
-server.use(cors(corsOptions))
+server.use(cors())
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
 server.use(
