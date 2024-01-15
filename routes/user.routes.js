7686c9101d0648823d5898a5f8b74d6b4bd7268c
rois90 <roissm90@gmail.com>
2024-01-15 16:16:35 +0100

    fix user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,58 +1,53 @@
-const mongoose = require('mongoose');
 const express = require('express');
 const passport = require('passport');
 const router = express.Router();
 const User = require('../models/User');
 const cors = require('cors');
 
+// Configuración CORS
 const corsOptions = {
     origin: '*', // Especifica el origen permitido
     credentials: true // Habilita las credenciales (si es necesario)
-  };
-  
+};
+
 router.use(cors(corsOptions));
 
-router.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', true);
-    next();
-});
-
+// Ruta para obtener todos los usuarios
 router.get('/', async (req, res, next) => {
     try {
         const users = await User.find();
-        return res.status(200).json(users)
-    }
-    catch (err) {
-        //return res.status(500).json(err)
-        next(err)
+        return res.status(200).json(users);
+    } catch (err) {
+        next(err);
     }
 });
 
-router.post('/register', async(req, res, next) => {
+// Ruta para registro de usuarios
+router.post('/register', async (req, res, next) => {
     try {
         const done = (error, user) => {
             if (error) {
                 console.log(error.message);
-                return next(error)
+                return next(error);
             }
             req.logIn(user, (error) => {
                 if (error) {
-                    next(error)
+                    next(error);
                 }
-                return res.status(201).json(user)
-            })
-        }
-        passport.authenticate('register', done)(req)
-    } catch  (err){
-        next(err)
+                return res.status(201).json(user);
+            });
+        };
+        passport.authenticate('register', done)(req);
+    } catch (err) {
+        next(err);
     }
 });
 
+// Ruta para iniciar sesión
 router.post('/login', (req, res, next) => {
     const done = (error, user) => {
-        if(error) {
-            return next(error)
+        if (error) {
+            return next(error);
         } else {
             req.login(user, (error) => {
                 if (error) {
@@ -60,20 +55,18 @@ router.post('/login', (req, res, next) => {
                 } else {
                     return res.status(200).json(user);
                 }
-            })
+            });
         }
-    }
-    passport.authenticate('login', done)(req)
-})
+    };
+    passport.authenticate('login', done)(req);
+});
 
+// Ruta para cerrar sesión
 router.post('/logout', (req, res, next) => {
-    console.log(req.session);
-    console.log(req.user);
-
     if (req.user) {
-        req.logout(err => { // Proporciona una función de devolución de llamada para req.logout()
+        req.logout(err => {
             if (err) {
-                return next(err); // Manejar errores, si los hay
+                return next(err);
             }
             req.session.destroy(() => {
                 res.clearCookie('connect.sid');
